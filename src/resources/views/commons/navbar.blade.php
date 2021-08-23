9b4ed0de0dcb953a17da4a4f0d8586e7e5ac924b
himekuri <himekuri9@gmail.com>
2021-08-23 14:33:34 +0900

    ヘッダーの選択状態を実装

--- src/resources/views/commons/navbar.blade.php
+++ src/resources/views/commons/navbar.blade.php
@@ -11,10 +11,9 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             @if (Auth::check())
                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">{!! link_to_route('items.index', '在庫確認', [], ['class' => 'nav-link']) !!}</li>
-                    <li class="nav-item">{!! link_to_route('categories.index', 'カテゴリー', [], ['class' => 'nav-link']) !!}</li>
-                    <li class="nav-item">{!! link_to_route('shops.index', '買い出し先', [], ['class' => 'nav-link']) !!}</li>
-                    <li class="nav-item">{!! link_to_route('lists.index', '買い出しリスト', [], ['class' => 'nav-link']) !!}</li>
+                    <li class="nav-item {{ request()->route()->named('items.index') ? 'active' : '' }}">{!! link_to_route('items.index', '在庫確認', [], ['class' => 'nav-link']) !!}</li>
+                    <li class="nav-item {{ request()->route()->named('categories.index') ? 'active' : '' }}">{!! link_to_route('categories.index', 'カテゴリー', [], ['class' => 'nav-link']) !!}</li>
+                    <li class="nav-item {{ request()->route()->named('shops.index') ? 'active' : '' }}">{!! link_to_route('shops.index', '買い出し先', [], ['class' => 'nav-link']) !!}</li>
                 </ul>
                 <ul class="navbar-nav">
                     <li class="dropdown-divider"></li>
